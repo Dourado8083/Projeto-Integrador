0a773614bf5c30dc26b99891d01c6621888ec20b
Eduardo Torquato <eduardotorquato78@gmail.com>
2021-08-11 15:54:28 -0300

    home-implement

--- Front-Ed/src/app/home/home.component.ts
+++ Front-Ed/src/app/home/home.component.ts
@@ -1,4 +1,11 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Message } from '../model/Message';
+import { Profile } from '../model/Profile';
+import { MessageService } from '../service/message.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +14,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  
 
-  ngOnInit(): void {
+  mesagem:string = "";
+  message: Message = new Message
+  messageList: Message[]
+  profile: Profile
+
+  profilee: Profile = new Profile()
+  profileId = environment.id
+
+  constructor(
+    private router: Router,
+    private messageService: MessageService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    if(environment.token==''){
+      this.router.navigate(['/entrar'])
+    }
+    this.findAllMessage()
   }
+  findAllMessage(){
+    this.messageService.getAllMessage().subscribe((resp: Message[])=>{
+      this.messageList=resp
+    })
+  }
+  send()
+  {
+    this.profile.profileId = environment.id;
+    this.message.profileOn = this.profile
+
+    console.log(environment.token)
+    console.log(environment.name)
+    this.messageService.postMessage(this.message).subscribe((resp)=>{
+        resp = this.message
+      alert('Mensagem cadastrada.')
+      this.findAllMessage()
+
+      this.message = new Message()
+    })
+  }
+ 
 
 }
--- Front-Ed/src/app/service/message.service.ts
+++ Front-Ed/src/app/service/message.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Message } from '../model/Message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set("Authorization", environment.token)
+  }
+
+  getAllMessage(): Observable<Message[]>
+  {
+    return this.http.get<Message[]>('http://localhost:8080/message/all', this.token)
+  }
+
+  postMessage(message: Message): Observable<Message>
+  {
+    return this.http.post<Message>('http://localhost:8080/message/post', message, this.token)
+  }
+  deleteMessage(messageId: number){
+    return this.http.delete(`http://localhost:8080/message/delete/${messageId}`, this.token)
+  }
+  getByIdMessage(messageId: number): Observable<Message>{
+    return this.http.get<Message>(`http://localhost:8080/message/${messageId}`, this.token) 
+  }
+  putMessage(message: Message): Observable<Message>{
+    return this.http.put<Message>('http://localhost:8080/message/edit', message, this.token)
+  }
 }
