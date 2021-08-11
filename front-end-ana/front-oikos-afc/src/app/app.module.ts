0d6cdbcd6fa25d9e0f247c453e4937e0a3497c69
unknown <anaflaviacamelorc@gmail.com>
2021-08-11 09:03:40 -0300

    home navbar e footer

--- front-end-ana/front-oikos-afc/src/app/app.module.ts
+++ front-end-ana/front-oikos-afc/src/app/app.module.ts
@@ -6,8 +6,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +16,9 @@ import { FooterComponent } from './footer/footer.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    ProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
