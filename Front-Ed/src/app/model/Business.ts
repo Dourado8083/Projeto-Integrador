a12e472415a373a1b09edc6c5ef13d9a3c225efa
Eduardo Torquato <eduardotorquato78@gmail.com>
2021-08-16 14:53:29 -0300

    .

---
+++ Front-Ed/src/app/model/Business.ts
@@ -0,0 +1,10 @@
+
+export class Business {
+    public businessId: number;
+    public businessName: string;
+    public businessAlias: string;
+    public businessEmail: string;
+    public businessPassword: string;
+    public businessBio: string;
+    public businessPic: string;
+}
