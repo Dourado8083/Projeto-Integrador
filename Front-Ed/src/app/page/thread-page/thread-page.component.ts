9c838338e3fb0c91879595930581d102ab79d6b8
Dourado8083 <gustavo.dourado.br@gmail.com>
2021-08-23 20:34:38 -0300

    Estilização do fórum

--- Front-Ed/src/app/page/thread-page/thread-page.component.ts
+++ Front-Ed/src/app/page/thread-page/thread-page.component.ts
@@ -1,4 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageDTO } from 'src/app/model/MessageDTO';
+import { Threads } from 'src/app/model/Threads';
+import { ThreadsDTO } from 'src/app/model/ThreadsDTO';
+import { MessageService } from 'src/app/service/message.service';
+import { ThreadsService } from 'src/app/service/threads.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-thread-page',
@@ -6,10 +14,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thread-page.component.css']
 })
 export class ThreadPageComponent implements OnInit {
+threadsid:number
+threadDTO:ThreadsDTO = new ThreadsDTO()
+thread:Threads = new Threads()
+messageDTO: MessageDTO = new MessageDTO()
+  constructor( private router:Router,
+  private http:HttpClient,
+  private activated:ActivatedRoute,
+  private threadService:ThreadsService,
+  private messageService:MessageService
 
-  constructor() { }
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
+    window.scroll(0,0)
+    
+    if (environment.token == '') {
+      this.router.navigate(['/entrar'])
+    }
   }
-
+getThreadsById(){
+  this.threadService.getThreadsById(this.threadsid).subscribe((resp:Threads)=>{
+  this.thread = resp
+  })
+}
+/*
+createmessage(){
+this.
+}
+*/
 }
