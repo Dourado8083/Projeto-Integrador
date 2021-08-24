5613af090bb8877b33ddb436a28fadd22776b205
unknown <anaflaviacamelorc@gmail.com>
2021-08-24 02:09:00 -0300

    Integração Ecommerce

--- Front-Ed/src/app/page/ecommerce-page/ecommerce-page.component.ts
+++ Front-Ed/src/app/page/ecommerce-page/ecommerce-page.component.ts
@@ -4,6 +4,7 @@ import { Ecommerce } from 'src/app/model/Ecommerce';
 import { Product } from 'src/app/model/Product';
 import { ProductDTO } from 'src/app/model/ProductDTO';
 import { EcommerceService } from 'src/app/service/ecommerce.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-ecommerce-page',
@@ -11,20 +12,26 @@ import { EcommerceService } from 'src/app/service/ecommerce.service';
   styleUrls: ['./ecommerce-page.component.css']
 })
 export class EcommercePageComponent implements OnInit {
-
+  
   ecommerce: Ecommerce = new Ecommerce();
   ecommerceId = this.activatedRoute.snapshot.params["id"];
 
   product: Product = new Product();
   productDto: ProductDTO = new ProductDTO();
 
+  pList: Product [];
+
   constructor(  
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private ecommerceService: EcommerceService
+    private ecommerceService: EcommerceService,
+    private productService: ProductService
   ) { }
 
   ngOnInit(): void {
+
+    window.scroll(0,0)
+
     this.getEcommerceById();
   }
 
@@ -34,6 +41,19 @@ export class EcommercePageComponent implements OnInit {
       this.ecommerce = resp;
       console.log(this.ecommerce);
     });
+  }
+
+  cadastrar(){
+    this.productDto.EcommerceOnId = this.ecommerceId
+    alert(this.ecommerceId)
+    alert(this.productDto.productName)
+    alert(this.productDto.productDescription)
+    alert(this.productDto.productPrice)
+
+    this.productService.registerProduct(this.productDto).subscribe((resp: Product)=>{
+      alert('foiiii')
+      this.productDto = new ProductDTO()
+    })
   }
 
 }
